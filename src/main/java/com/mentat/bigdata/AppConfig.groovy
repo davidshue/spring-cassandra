00401543f41464c00b784f3925022ed994a15a38
David Shue <dshue1@babycenter.com>
2014-11-15 11:38:22 -0800

    added test and javaconfig for the framework

---
+++ src/main/java/com/mentat/bigdata/AppConfig.groovy
@@ -0,0 +1,71 @@
+package com.mentat.bigdata
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.core.io.ClassPathResource
+import org.springframework.data.cassandra.config.CassandraClusterFactoryBean
+import org.springframework.data.cassandra.config.CassandraSessionFactoryBean
+import org.springframework.data.cassandra.config.SchemaAction
+import org.springframework.data.cassandra.convert.CassandraConverter
+import org.springframework.data.cassandra.convert.MappingCassandraConverter
+import org.springframework.data.cassandra.core.CassandraOperations
+import org.springframework.data.cassandra.core.CassandraTemplate
+import org.springframework.data.cassandra.mapping.BasicCassandraMappingContext
+import org.springframework.data.cassandra.mapping.CassandraMappingContext
+
+import com.datastax.driver.core.Cluster
+import com.datastax.driver.core.Session
+
+@Configuration
+class AppConfig {
+	@Bean
+	PropertyPlaceholderConfigurer configProperties() {
+		PropertyPlaceholderConfigurer props = new PropertyPlaceholderConfigurer(
+			location: new ClassPathResource('bigdata.properties')
+		)
+	}
+	
+	@Bean
+	CassandraClusterFactoryBean cluster(@Value('${cassandra.contactpoints}') String contactPoints,
+			@Value('${cassandra.port}') String cassandraPort) {
+
+		CassandraClusterFactoryBean cluster = new CassandraClusterFactoryBean();
+		cluster.setContactPoints(contactPoints);
+		cluster.setPort(Integer.parseInt(cassandraPort));
+
+		return cluster;
+	}
+
+	@Bean
+	public CassandraMappingContext mappingContext() {
+		return new BasicCassandraMappingContext();
+	}
+
+	@Bean
+	public CassandraConverter converter() {
+		return new MappingCassandraConverter(mappingContext());
+	}
+
+	@Bean
+	@Autowired
+	public CassandraSessionFactoryBean session(Cluster cluster,
+			@Value('${cassandra.keyspace}') String keyspace) throws Exception {
+
+		CassandraSessionFactoryBean session = new CassandraSessionFactoryBean();
+		session.setCluster(cluster);
+		session.setKeyspaceName(keyspace);
+		session.setConverter(converter());
+		session.setSchemaAction(SchemaAction.NONE);
+
+		return session;
+	}
+
+	@Bean
+	@Autowired
+	public CassandraOperations cassandraTemplate(Session session) throws Exception {
+		return new CassandraTemplate(session);
+	}
+}
---
+++ src/test/java/com/mentat/bigdata/BigDataTest.groovy
@@ -0,0 +1,38 @@
+package com.mentat.bigdata;
+
+import static org.junit.Assert.*
+
+import org.apache.cassandra.utils.UUIDGen
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.cassandra.core.CassandraOperations
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
+
+import com.datastax.driver.core.Session
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes=AppConfig.class)
+@ActiveProfiles("bigdata")
+class BigDataTest {
+
+	static final String NOTIFICATION_TABLE = "notification"
+	
+	@Autowired
+	private Session session
+
+	@Autowired
+	private CassandraOperations cassandraOps
+
+
+
+	@Test
+	void testTimeUUID() {
+		UUID id = UUIDGen.timeUUID
+		println 'time: ' + new Date(UUIDGen.getAdjustedTimestamp(id))
+	}
+
+
+}
